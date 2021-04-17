d33bac96dccb3fb4e063128bb3cc43f831e3b60d
Laurent Tréguier <laurent@treguier.org>
2021-04-17 12:16:35 +0200

    Optimize format command

--- core/management/commands/format.py
+++ core/management/commands/format.py
@@ -3,27 +3,29 @@ from os.path import dirname, join
 from pathlib import Path
 
 import isort
-from black import FileMode, WriteBack, format_file_in_place
+from black import FileMode, Report, WriteBack, reformat_many
 from django.core.management.base import BaseCommand
 from isort.wrap_modes import WrapModes
 from tqdm import tqdm
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         management_dir = dirname(dirname(__file__))
         project_dir = dirname(dirname(management_dir))
+        files = glob(join(project_dir, "**", "*.py"), recursive=True)
         config = isort.Config(
             quiet=True,
             multi_line_output=WrapModes.VERTICAL_HANGING_INDENT,
         )
-        mode = FileMode()
 
-        for file in tqdm(glob(join(project_dir, "**", "*.py"), recursive=True)):
+        for file in tqdm(files):
             isort.file(file, config=config)
-            format_file_in_place(
-                Path(file),
-                fast=True,
-                mode=mode,
-                write_back=WriteBack.YES,
-            )
+
+        reformat_many(
+            sources=set([Path(f) for f in files]),
+            fast=True,
+            write_back=WriteBack.YES,
+            mode=FileMode(),
+            report=Report(quiet=True),
+        )
