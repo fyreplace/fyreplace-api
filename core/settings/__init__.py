958822ed42293314690157c2c39f75aea36a510f
Laurent Tréguier <laurent@treguier.org>
2021-07-03 20:39:30 +0200

    Remove forgotten middleware

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "crum.CurrentRequestUserMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
