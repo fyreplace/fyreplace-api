f45fb408e3aa7b7cb3c89da2c9d8ee6517c98941
Laurent Tréguier <laurent@treguier.org>
2021-08-12 17:42:00 +0200

    Don't bother configuring the JWT algorithm

--- core/jwt.py
+++ core/jwt.py
@@ -7,7 +7,7 @@ def encode(payload: dict) -> str:
     return j.encode(
         payload=payload,
         key=settings.SECRET_KEY,
-        algorithm=settings.JWT_ALGORITHM,
+        algorithm="HS256",
     )
 
 
@@ -15,5 +15,5 @@ def decode(token: str) -> dict:
     return j.decode(
         jwt=token,
         key=settings.SECRET_KEY,
-        algorithms=[settings.JWT_ALGORITHM],
+        algorithms=["HS256"],
     )
