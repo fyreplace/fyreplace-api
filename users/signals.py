830b2968b7cbd3133201eb25a52d9ed4c5b35f9b
Laurent Tréguier <laurent.treguier@fyreplace.app>
2022-06-26 20:00:11 +0200

    Fix ban lifting

--- users/signals.py
+++ users/signals.py
@@ -13,7 +13,7 @@ post_ban = ModelSignal(use_caching=True)
 
 
 @receiver(post_save, sender=get_user_model())
-def on_user_post_save(instance: AbstractUser, created: bool, **kwargs):
+def on_user_post_save(instance: AbstractUser, **kwargs):
     get_user_model().objects.filter(
         id=instance.id, is_deleted=True, is_active=True
     ).update(is_active=False)
@@ -24,8 +24,10 @@ def on_user_post_ban(instance: AbstractUser, **kwargs):
     remove_user_data.delay(user_id=str(instance.id))
     send_user_banned_email.delay(instance.id)
 
-    if end := instance.date_ban_end and not settings.IS_TESTING:
-        lift_ban.apply_async(kwargs={"user_id": str(instance.id)}, eta=end)
+    if instance.date_ban_end and not settings.IS_TESTING:
+        lift_ban.apply_async(
+            kwargs={"user_id": str(instance.id)}, eta=instance.date_ban_end
+        )
 
 
 @receiver(post_delete, sender=get_user_model())
